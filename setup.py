37287a0ccebb3c0e010027903f7e6b30b325db8e
shashikg <shashikg.iitk@gmail.com>
2020-02-26 13:01:21 +0530

    changes to setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,50 @@
+## >>>>>> Original MATLAB exported code for setup.py >>>>>>>>>>>>>>>>>>>>>>>>>>>
+#
 # Copyright 2015-2018 The MathWorks, Inc.
 
-from distutils.core import setup
-from distutils.command.clean import clean
-from distutils.command.install import install
+# from distutils.core import setup
+# from distutils.command.clean import clean
+# from distutils.command.install import install
+#
+# class InstallRuntime(install):
+#     # Calls the default run command, then deletes the build area
+#     # (equivalent to "setup clean --all").
+#     def run(self):
+#         install.run(self)
+#         c = clean(self.distribution)
+#         c.all = True
+#         c.finalize_options()
+#         c.run()
+#
+# if __name__ == '__main__':
+#
+#     setup(
+#         name="matlabruntimeforpython",
+#         version="R2019b",
+#         description='A module to call MATLAB from Python',
+#         author='MathWorks',
+#         url='https://www.mathworks.com/',
+#         platforms=['Linux', 'Windows', 'MacOS'],
+#         packages=[
+#             'ScanMatchPy'
+#         ],
+#         package_data={'ScanMatchPy': ['*.ctf']},
+#         # Executes the custom code above in order to delete the build area.
+#         cmdclass={'install': InstallRuntime}
+#     )
+#
+## >>>>> Original MATLAB exported code for setup.py >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-class InstallRuntime(install):
-    # Calls the default run command, then deletes the build area 
-    # (equivalent to "setup clean --all").
-    def run(self):
-        install.run(self)
-        c = clean(self.distribution)
-        c.all = True
-        c.finalize_options()
-        c.run()
+# Conversion to use setuptools instead of disutils to make it compatible with pip
 
-if __name__ == '__main__':
+from setuptools import setup, find_packages
 
-    setup(
-        name="matlabruntimeforpython",
-        version="R2019b",
-        description='A module to call MATLAB from Python',
-        author='MathWorks',
-        url='https://www.mathworks.com/',
-        platforms=['Linux', 'Windows', 'MacOS'],
-        packages=[
-            'ScanMatchPy'
-        ],
-        package_data={'ScanMatchPy': ['*.ctf']},
-        # Executes the custom code above in order to delete the build area.
-        cmdclass={'install': InstallRuntime}
-    )
-
-
+setup(
+    name="ScanMatchPy",
+    version="1",
+    description='A python converted module of ScanMatch Toolbox for Matlab',
+    packages=[
+        'ScanMatchPy'
+    ],
+    package_data={'ScanMatchPy': ['*.ctf']},
+)
